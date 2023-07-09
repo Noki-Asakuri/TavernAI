51fa1740099ab98c9835fa344edc2e4038ea8b3f
Asakuri <phungtanphat23@gmail.com>
2023-07-09 21:36:41 +0700

    feat: Add method to resize all textareas on first launch

--- public/class/UIMasterSettings.mjs
+++ public/class/UIMasterSettings.mjs
@@ -1,7 +1,10 @@
+import { textareaAutosize } from "../script.js";
 import { Resizable } from "./Resizable.mjs";
 import { UIFactory } from "./UIFactory.mjs";
 
 export class UIMasterSettings extends Resizable {
+	resize_on_first_launch = true;
+
 	/**
 	 * Creates modular, resizable window in given container. Root is a "shadow" (taking up the whole screen), its sole div .container child is the window itself.
 	 * @param {{ root: Element }} options
@@ -20,5 +23,22 @@ export class UIMasterSettings extends Resizable {
 			right,
 			bottom,
 		});
+	}
+
+	ResizeAllTextArea() {
+		if (!this.resize_on_first_launch) return;
+
+		const all_textarea = [
+			$("#system_prompt_textarea"),
+			$("#jailbreak_prompt_textarea"),
+			$("#user_jailbreak_prompt_textarea"),
+			$("#nsfw_encouraged_prompt_textarea"),
+			$("#nsfw_avoidance_prompt_textarea"),
+			$("#impersonate_prompt_textarea"),
+		];
+
+		all_textarea.map((el) => textareaAutosize(el));
+
+		this.resize_on_first_launch = false;
 	}
 }
